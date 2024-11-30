ebb934b8a3e582983e7a50a093c22a50dd33b8fb
RAQUEL BRENA <raquel.lima.124@ufrn.edu.br>
2024-11-29 22:20:12 -0300

    Add AppRoutes class for managing application navigation routes

---
+++ f08_eshop_app/lib/utils/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const PRODUCT_DETAIL = '/product-detail';
+  static const PRODUCT_FORM = '/product-form';
+  static const CART = '/cart';
+  static const ORDERS = '/orders';
+}
